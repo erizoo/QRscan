9b7516865a497035b160c845f2e58093c31ed7fe
erizo <alexboiko1993@gmail.com>
2018-09-09 14:54:45 +0300

    add

---
+++ app/src/main/java/by/erizo/scan/qrscan/CaptureActivityPortrait.java
@@ -0,0 +1,7 @@
+package by.erizo.scan.qrscan;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+
+}
--- app/src/main/java/by/erizo/scan/qrscan/MainActivity.java
+++ app/src/main/java/by/erizo/scan/qrscan/MainActivity.java
@@ -1,24 +1,66 @@
 package by.erizo.scan.qrscan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 import by.erizo.scan.qrscan.ui.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+    private IntentIntegrator integrator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getScreenComponent().inject(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setOrientationLocked(true);
+        integrator.setBeepEnabled(false);
+        integrator.setCaptureActivity(CaptureActivityPortrait.class);
+        integrator.setBarcodeImageEnabled(false);
+
     }
+
+    @OnClick(R.id.without_code_button)
+    public void startScan() {
+        integrator.initiateScan();
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                integrator.initiateScan();
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
